032654efd8e03a43111360b90bf5d6d7427335b7
Aleksandr Glek <avglek@gmail.com>
2025-03-19 20:11:14 +0300

    step 2

--- some-file.py
+++ some-file.py
@@ -1,2 +1,9 @@
 print ("test repository")
 print ("test")
+
+def say_something(number:int, word:str)->str:
+    word = word.capitalize()
+    return word * number
+
+print("Echo "+say_something(5,"test"))
+
